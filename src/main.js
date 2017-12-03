d5e403e4e5be9d3159c8972f6b2c64956f06da8e
javeedishaq <ishaqjaveed1@gmail.com>
2017-12-04 01:59:13 +0500

    completed project

---
+++ src/main.js
@@ -0,0 +1,23 @@
+/*** src/main.js ***/
+
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+
+import ShoppingList from './screens/ShoppingList';
+import AddProduct from './screens/AddProduct';
+
+const Navigator = StackNavigator({
+    ShoppingList: { screen: ShoppingList },
+    AddProduct: { screen: AddProduct}
+});
+
+export default class App extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+
+        return <Navigator />;
+    }
+}
